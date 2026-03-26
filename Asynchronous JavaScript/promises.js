8f4e02e8a9baea527f145215c2f3568ba09e1153
Error_404 <built.different7813@gmail.com>
2026-03-26 10:18:39 +0530

    Add files via upload

---
+++ Asynchronous JavaScript/promises.js
@@ -0,0 +1,8 @@
+// 1. Promises are very important topic in javaScript.
+
+// 2. There are 3 states of Promises.
+// 2a). Pending state
+// 2b). Fulfilled state
+// 2c). Rejected state
+
+// 3. Promises are said to be settled, when, it is in one of the two states, either in Fulfilled state or in Rejected state.
