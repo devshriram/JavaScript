31b64177c2e9e83b6c10f293811bc8276d4f753c
Curious Developer <built.different7813@gmail.com>
2026-04-01 14:17:47 +0530

    Add files via upload

---
+++ Asynchronous JavaScript/setTimeout.js
@@ -0,0 +1,17 @@
+// Example - 1
+console.log(1);
+
+setTimeout(()=>{
+    console.log(2);
+}, 1000);
+
+console.log(3);
+
+// Example - 2
+console.log(1);
+
+setTimeout(()=>{
+    console.log(2);
+},0);
+
+console.log(3);
