3eca0b8f6326cc4220cf21b22287623a331f1c74
Curious Developer <built.different7813@gmail.com>
2026-04-20 09:44:46 +0530

    Add files via upload

---
+++ Building a Todo list using APIs/startingWithProject.js
@@ -0,0 +1,18 @@
+// UI
+
+// Functionality
+// .Adding a TODO
+// .Delete a TODO
+// .Check Task
+// .Total items count
+
+// Data
+// .tasks -> [{},{}]
+// .task items -> {id,text,done}
+
+// Functions
+// .add TODO
+// .delete TODO
+// .check TODO
+// .show Notofication
+// .render List
