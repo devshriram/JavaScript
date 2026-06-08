7dfd6d03fd81aed908fc761fd0c8fc274e69a714
Curious Developer <built.different7813@gmail.com>
2026-06-08 09:37:10 +0530

    Add files via upload

---
+++ Promises/chainingRequests.js
@@ -0,0 +1,16 @@
+/*
+ $.ajax({
+    success: function() {
+    $.ajax({
+    success: function() {
+    $.ajax({
+    })
+    }
+    })
+    }
+ })
+*/
+
+/*
+promise1.then().then().then().catch();
+*/
