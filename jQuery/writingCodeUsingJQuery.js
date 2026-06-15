683eb021b07170352bc4ee737c76f0d3fba44a00
Curious Developer <built.different7813@gmail.com>
2026-06-15 10:13:41 +0530

    Add files via upload

---
+++ jQuery/writingCodeUsingJQuery.js
@@ -0,0 +1,10 @@
+var para1 = document.getElementById('para1');
+para1.innerHTML = "Welcome";
+
+$('#para1').hide();
+
+$('#para1').html('Welcome');
+
+$('#para1').width(200);
+
+$('p').html('Welcome');
