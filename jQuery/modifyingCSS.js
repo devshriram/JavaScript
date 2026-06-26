06a62ea30adcd8d465739855be4540357ead204c
Curious Developer <built.different7813@gmail.com>
2026-06-26 15:36:10 +0530

    Add files via upload

---
+++ jQuery/modifyingCSS.js
@@ -0,0 +1,15 @@
+$('#para1').css('color', 'red');
+$('#para2').css('color', 'green');
+
+$('p').css('fontSize', '30px');
+
+$('p').css({
+    fontSize: "30px",
+    color: "purple"
+});
+
+$('div').css({
+    height: "100px",
+    width: "100px",
+    backgroundColor: "green",
+});
