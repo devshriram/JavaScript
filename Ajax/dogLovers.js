9452f24bd5e87b77d50ba9a6cbe5a2b8012b831c
Built Different <built.different7813@gmail.com>
2026-08-19 16:42:29 +0530

    Enhance dog image fetching with breed selection

--- Ajax/dogLovers.js
+++ Ajax/dogLovers.js
@@ -1,38 +1,62 @@
-function fetchRandomDogImage() {
+// Fetch all the Breed 
+$.get('https://dog.ceo/api/breeds/list/all', function(data){
+    for (let i in data.message) {
+     var newOption = $('<option>');
+     newOption.text(i);
+     newOption.val(i);
+     $('#breed_selected').append(newOption);
+    }
 
-    // Get the Selected Breed
-    var selectedValue = $('#breed_selected').val();
-    
-    // Initialize the HTTP Request
-    var xhrRequest = new XMLHttpRequest();
-
-    // Open the Connection with Server
-    xhrRequest.open('GET', `https://dog.ceo/api/breed/${selectedValue}/images/random`, true);
-
-    // Makes the Request to the Server
-    xhrRequest.send();
-
-    // Handle the Response from the Server
-    xhrRequest.onload = function() {
-        var responseJSON = JSON.parse(xhrRequest.response);
-        var imageUrl = responseJSON.message;
-        $('#dog-image').attr('src', imageUrl);
-    };
-
-    // Handle the Error
-    xhrRequest.onerror = function() {
-        console.log("Request Failed");
-    };
-};
-
-$('#get-dog-image-button').click(fetchRandomDogImage);
-
-$('#next-image-button').click(function() {
-
-    var selectedValue = $('#breed_selected').val();
-
-    $.get(`https://dog.ceo/api/breed/${selectedValue}/images/random`, function(data) {
-        var imageUrl = data.message;
-        $('#dog-image').attr('src', imageUrl);
+    // Handle the Change in the Dropdown List
+    $('#breed_selected').change(function() {
+        // Get the Selected Value
+        let selectedBreed = $('#breed_selected').val();
+        // Get sub-breed array
+        let breedArray = data.message[selectedBreed];
+        // Remove the existing sub-breed
+        $('#dynamicForm').remove();
+        // Check if the sub-breed is present or not
+        if(breedArray.length==0) {
+            return;
+        }
+        // Make Dropdown List for sub-breed
+        let form = $(document.createElement('form')).attr('id', 'dynamicForm');
+        let select = $(document.createElement('select')).attr('id', 'dynamicOption').css({
+            display: 'inline-block',
+            position: 'fixed',
+            top: '10vh',
+            left: '50vw',
+            fontSize: '18px',
+            padding: '2px',
+            paddingRight: '10px',
+        });
+        for (let j in breedArray) {
+            var latestOption = $('<option>');
+            latestOption.text(breedArray[j]);
+            latestOption.val(breedArray[j]);
+            $(select).append(latestOption);
+        };
+        form.append(select);
+        $('#header').append(form);
     });
-});
+}); 
+   
+   function fetchDogImage() {
+
+    // Clear the existing Image Container
+    $('#image-container').empty();
+
+    // Get the Selected Breed Value
+   var selectedDogBreed = $('#breed_selected').val();
+
+    // Call the API to get selected Breed
+    $.get(`https://dog.ceo/api/breed/${selectedDogBreed}/images`, function(data) {
+           var imageUrlArray = data.message;
+           for (let k in imageUrlArray) {
+           var image =  $(document.createElement('img')).attr('src', imageUrlArray[k]);
+           $('#image-container').append(image);
+           }
+    });
+   }
+   
+$('#get-image-button').click(fetchDogImage);
