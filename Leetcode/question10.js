2c45b3bc76dd7765760a669849b7d1a6a39b5b16
Built Different <built.different7813@gmail.com>
2026-08-26 11:35:01 +0530

    Add files via upload

---
+++ Leetcode/question10.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    
+    var cnt=1;
+
+    return function(...args){
+        if(cnt==1) {
+            cnt++;
+            return fn(...args);
+        } else {
+            return undefined;
+        }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
